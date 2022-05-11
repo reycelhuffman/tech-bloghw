3cca01ec64e1a8cb145d1826cf332213a4853b38
Reycel Huffman <reycel.huffman@outlook.com>
2022-05-10 20:26:51 -0500

    finished models

--- models/index.js
+++ models/index.js
@@ -1,13 +1,32 @@
 const User = require('./User');
 const Project = require('./Post');
 
-User.hasMany(this.Post, {
+User.hasMany(Post, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+User.hasMany(Comment, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
 
 Post.belongsTo(User, {
   foreignKey: 'user_id'
+  onDelete: 'CASCADE'
 });
 
-module.exports = { User, Post };
+Post.hasMany(Comment, {
+  foreignKey: 'post_id',
+  onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Comment };
