413a925580be989b87aab9b0e5a5470b7ec787e2
Reycel Huffman <reycel.huffman@outlook.com>
2022-06-09 14:53:37 -0500

    edit homeRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -114,7 +114,7 @@ router.get('/newpost', async (req, res) => {
     const posts = postData.map((post) => post.get({ plain: true }));
 
     // Pass serialized data and session flag into template
-    res.render('newpost', { 
+    res.render('newPost', { 
       posts, 
       logged_in: req.session.logged_in 
     });
